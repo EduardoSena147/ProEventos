d5a5c114a752db79fd840b935c4f07bc7ced9242
Eduardo Sena Cavalcante <eduardosena147@outlook.com>
2024-09-06 15:25:15 -0300

    Criação da API em .Net 5.0

--- Back/src/ProEventos.API/Startup.cs
+++ Back/src/ProEventos.API/Startup.cs
@@ -35,6 +35,7 @@ namespace ProEventos.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Comentário apenas para salvar novo commit
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
